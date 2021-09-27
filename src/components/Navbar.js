8a8c906cb6167b6541cb1904e4d86240cd6aaf93
moraelhumano <moraelhumano@gmail.com>
2021-09-26 23:15:39 -0500

    se cambia ruta calendario en navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,10 +15,13 @@ class NavBar extends Component{
     render() {
         return (
         <>
-        <nav className="md:fixed w-full z-30 top-0 shadow-navbar md:pl-20 mx-auto flex py-2 justify-center md:justify-between">
-            <div className="flex items-center  ">
+        <nav className="md:fixed w-full z-30 top-0 shadow-navbar px-12 md:px-20 mx-auto flex py-2 justify-center md:justify-between">
+            <div className="flex w-full items-center justify-between">
                 <Link to="/" className="toggleColour flex text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
                     <img className="h-12 items-center" src={LogoRober} alt="" />
+                </Link>
+                <Link to="/calendario" className="toggleColour flex text-white no-underline hover:no-underline font-bold">
+                    <h2 className="text-base lg:text-lg">FECHAS</h2>
                 </Link>
             </div>
         </nav>
