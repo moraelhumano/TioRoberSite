ac78dd2428d17f50b812595a0b07325f689823e6
unknown <salvador.mora@mattilda.io>
2023-05-24 09:16:44 -0700

    se hacen cambios en el hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import 'reactjs-popup/dist/index.css';
 import Fade from 'react-reveal/Fade';
+import RubberBand from 'react-reveal/RubberBand';
+
 
 import OKEsta from '../../src/okestabien.png';
 
-class HeroHome extends Component{
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import Zoom from 'react-reveal/Zoom';
+import Bounce from 'react-reveal/Zoom';
+
+import BgTioHome from '../../src/TioHomeHero.jpg'
+
+class HeroHome extends Component {
     render() {
         return (
-        <>
-        <Fade>
-        <div className="md:pt-24 hero-home md:h-screen flex-col">
-            <div className="container p-10 md:h-full px-3 mx-auto flex flex-col background-rober justify-center">
-                <div className="flex flex-col justify-center md:justify-left text-center md:text-left w-full md:w-8/12">
-                    <p className="uppercase text-4xl md:text-5xl tracking-loose md:pl-4 text-center md:text-left" 
-                    style={{  fontWeight: '900',
-                                textShadow: '-3px 2px black',
-                                }}
-                    >Nominada al Ariel</p>
-                <div className="w-full flex justify-center md:justify-left">
-                <img className="w-3/4 mt-4 md:mt-0" src={OKEsta} />  
-                </div>
-                </div>
-                <a className="self-center" target="_blank" href="https://player.vimeo.com/video/569135029">
-                    <button class="mx-auto lg:mx-0 font-bold py-2 px-4 border button-home content-center mt-2 rounded">
-                        MÍRALA EN VIMEO
-                    </button>
-                </a>
-            </div>
-        </div>
-        </Fade>
-        </>
+            <>
+                <Fade>
+                    <section className="md:pt-24 hero-home h-screen flex-col flex justify-center pl-20">
+                        <div className="sm:text-center ">
+                            <div className="text-center lg:text-left">
+                                <p className="text-4xl tracking-tight font-extrabold text-white sm:text-3xl md:text-3xl text-shadow-lg">Roberto Andrade</p>
+                                <p className="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl text-shadow-lg">EL TÍO ROBER</p>
+                            </div>
+                            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                <div className="rounded-md shadow">
+                                    <Link className="w-full flex items-center justify-center px-8 py-3 border border-white text-base font-medium rounded-md text-white bg-blue  md:py-4 md:text-lg md:px-10" to="/calendario">Encuentra tus boletos</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </Fade>
+            </>
 
         )
     }
