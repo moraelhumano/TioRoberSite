89e12a6da3e4c7b7e4f24cc3f65cf09b9aa76827
unknown <salvador.mora@mattilda.io>
2023-05-30 11:13:18 -0700

    se agrega responsivo a banner de pepsi center

--- src/components/PepsiCenterBanner.js
+++ src/components/PepsiCenterBanner.js
@@ -14,7 +14,7 @@ class PepsiCenterBanner extends Component {
             <>
                 <a className='z-10 fixed' href="https://www.eticket.mx/masinformacion.aspx?IdEvento=30710" target="_blank">
                     <div className="bg-blue-pepsi flex items-center justify-center">
-                        <img className="logo-rey w-1/4" src={LaHoraPepsiCenter} />
+                        <img className="logo-rey sm:w-1/2 md:w-1/4" src={LaHoraPepsiCenter} />
                     </div>
                 </a>
             </>
