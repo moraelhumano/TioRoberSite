3e06a811b4da052a9eaa2290d516ca5bf10b6430
unknown <salvador.mora@mattilda.io>
2023-05-31 17:18:36 -0700

    se modifican estilos de sección de bio del tío

--- src/components/Shows.js
+++ src/components/Shows.js
@@ -48,7 +48,7 @@ class Shows extends Component {
                     <Zoom>
                         <div className="title-section">
                             <h3 className="text-2xl md:text-3xl text-white text-center text-white font-bold leading-none">
-                                PRÓXIMAS FECHAS
+                                PRÓXIMOS SHOWS
                             </h3>
                         </div>
                     </Zoom>
